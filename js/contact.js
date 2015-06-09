d6dc3e91738759ab6946fc03ea635d2263bc15a0
Nagy Richard <nr@ecostudio.hu>
2015-06-09 13:20:50 +0200

    Created and styled upload control based on Dropzone.

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,53 @@
-define(['jquery', 'underscore'], function ($, _) {
+define(['jquery', 'underscore', 'dropzone'], function ($, _, Dropzone) {
+
+	var dzTemplate =
+		'<div class="dz-preview dz-file-preview">' +
+			'<div class="dz-details">' +
+				'<span class="dz-filename" data-dz-name></span>' +
+				'<span class="dz-size" data-dz-size></span>' +
+			'</div>' +
+			' <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>' +
+			'<div class="dz-success-mark"></div>' +
+			'<div class="dz-error-mark"></div>' +
+			'<div class="error" data-dz-errormessage></div>' +
+			'<button type="button" class="remove" data-dz-remove title="Törlés"></button' +
+		'</div>';
+
+	Dropzone.autoDiscover = false;
 
 	return function (selector) {
 
 		var $formCont = $(selector);
 		var $form = $formCont.find('form');
+
+		var $dzone = $form.find('.contact-dropzone').show();
+		if ($dzone.length) {
+			var dz = new Dropzone($dzone.get(0), {
+				url: 'fileupload',
+				previewTemplate: dzTemplate,
+				maxFilesize: 2,
+				createImageThumbnails: false,
+				maxFiles: 5,
+				acceptedFiles: [
+					'image/*',
+					'application/pdf',
+					'application/msword',
+					'application/rtf',
+					'text/richtext',
+					'text/plain',
+					'text/xml',
+					'application/xml'
+				].join(','),
+				dictInvalidFileType: 'Nem engedélyezett fájltípus.',
+				dictFileTooBig: 'Túl nagy fájl, {{maxFilesize}} MB a maximum.',
+				dictResponseError: 'Feltöltési hiba.',
+				dictMaxFilesExceeded: 'Több fájlt nem tölthetsz fel.'
+			});
+
+			dz.on('success', function (file, response) {
+				$(file.previewElement).data('fileid', response);
+			});
+		}
 
 		$form
 			.on('submit', function (e) {
@@ -12,7 +56,16 @@ define(['jquery', 'underscore'], function ($, _) {
 				var $labels = $form.find('label').removeClass('has-error');
 
 				var data = $form.serializeArray();
-				data.push({ name: 'referrer', value: document.referrer });
+				data.push({
+					name: 'referrer',
+					value: document.referrer
+				},
+				{
+					name: 'files',
+					value: JSON.stringify($form.find('.dropzone .dz-preview').map(function () {
+						return $(this).data('fileid');
+					}).get())
+				});
 
 				$.post(window.location, data, function(response) {
 					if (_(response).isEmpty()) {
