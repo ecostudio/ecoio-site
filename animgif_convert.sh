16535eb5e16b9b4cc3a826cdc37f0a7aeddae6db
Nagy Richard <nr@ecostudio.hu>
2015-07-16 18:20:27 +0200

    Added the script that is used to generate mp4, webm and jpg from the animated android gifs.

---
+++ animgif_convert.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+find image/android -name "*.gif" | xargs -I {file} ffmpeg -i {file} -an -vcodec libvpx -f webm {file}.webm
+find image/android -name "*.gif" | xargs -I {file} ffmpeg -i {file} -an -vcodec libx264 -pix_fmt yuv420p -profile:v main -level 31 -f mp4 {file}.mp4
+find image/android -name "*.gif" | xargs -I {file} ffmpeg -i {file} -vframes 1 {file}.jpeg
