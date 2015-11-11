cd36a08b2d41da6aeefd47955c0313c2723a2524
Varga Bence <vb@ecostudio.hu>
2015-11-11 12:23:17 +0100

    Avatar converter updated to match the 'Ricsi' convention.

--- animgif_convert.sh
+++ animgif_convert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find image/android -name "*.gif" | xargs -I {file} ffmpeg -i {file} -an -vcodec libvpx -f webm {file}.webm
-find image/android -name "*.gif" | xargs -I {file} ffmpeg -i {file} -an -vcodec libx264 -pix_fmt yuv420p -profile:v main -level 31 -f mp4 {file}.mp4
-find image/android -name "*.gif" | xargs -I {file} ffmpeg -i {file} -vframes 1 {file}.jpeg
+find image/android -name "*.gif" | sed -r 's/.gif$//g' | xargs -I {file} ffmpeg -i {file} -an -vcodec libvpx -f webm {file}.webm
+find image/android -name "*.gif" | sed -r 's/.gif$//g' | xargs -I {file} ffmpeg -i {file} -an -vcodec libx264 -pix_fmt yuv420p -profile:v main -level 31 -f mp4 {file}.mp4
+find image/android -name "*.gif" | sed -r 's/.gif$//g' | xargs -I {file} ffmpeg -i {file} -vframes 1 {file}.jpeg
