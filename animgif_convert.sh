d14ff421ff11a2a8590e8b6e26d9d8565e78303f
Péter Stefánia <ps@ecostudio.hu>
2015-11-11 15:21:07 +0100

    Convert script fixed, changed naming convetion.

--- animgif_convert.sh
+++ animgif_convert.sh
@@ -2,4 +2,4 @@
 
 find image/android -name "*.gif" | sed -r 's/.gif$//g' | xargs -I {file} ffmpeg -i {file}.gif -an -vcodec libvpx -f webm {file}.webm
 find image/android -name "*.gif" | sed -r 's/.gif$//g' | xargs -I {file} ffmpeg -i {file}.gif -an -vcodec libx264 -pix_fmt yuv420p -profile:v main -level 31 -f mp4 {file}.mp4
-find image/android -name "*.gif" | sed -r 's/.gif$//g' | xargs -I {file} ffmpeg -i {file}.gif -vframes 1 {file}.jpeg
+find image/android -name "*.gif" | sed -r 's/.gif$//g' | xargs -I {file} ffmpeg -i {file}.gif -vframes 1 {file}.jpg
